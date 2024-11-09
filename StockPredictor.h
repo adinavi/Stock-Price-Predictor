5a00e435be96db129847275e82062a5945a87574
Maxwell Adinavi <85236017+adinavi@users.noreply.github.com>
2024-11-09 11:30:09 +0000

    Update StockPredictor.h

--- StockPredictor.h
+++ StockPredictor.h
@@ -1,1 +1,19 @@
+#ifndef STOCKPREDICTOR_H
+#define STOCKPREDICTOR_H
 
+#include <vector>
+#include <string>
+
+class StockPredictor {
+public:
+    void loadData(const std::string& filename);
+    void train();
+    double predictNextPrice() const;
+
+private:
+    std::vector<double> prices;
+    double slope = 0.0;
+    double intercept = 0.0;
+};
+
+#endif
